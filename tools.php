01a6e78b1613784bbfbdded827d2747b688c016b
BPTproto <77068386+BPTproto@users.noreply.github.com>
2022-06-18 15:51:53 +0430

    initial commit3
    I forgot to delete test code :D

--- tools.php
+++ tools.php
@@ -224,45 +224,4 @@ class tools {
         }
         else return false;
     }
-
-    public static function error2trace (\Throwable $error,$seen = null): string {
-        $starter = $seen ? 'Caused by: ' : '';
-        $result = [];
-        if (!$seen) $seen = [];
-        $trace = $error->getTrace();
-        $prev = $error->getPrevious();
-        //$result[] = sprintf('%s%s: %s', $starter, get_class($error), $error->getMessage());
-        $file = $error->getFile();
-        $line = $error->getLine();
-
-        while (true){
-            $current = "$file:$line";
-            if (is_array($seen) && in_array($current, $seen)) {
-                $result[] = sprintf(' ... %d more', count($trace) + 1);
-                break;
-            }
-            $result[] = sprintf(' at %s%s%s(%s%s%s)',
-                count($trace) && array_key_exists('class', $trace[0]) ? $trace[0]['class'] : '',
-                count($trace) && array_key_exists('class', $trace[0]) && array_key_exists('function', $trace[0]) ? (isset($trace[0]['type']) ? $trace[0]['type'] : '.') : '',
-                count($trace) && array_key_exists('function', $trace[0]) ? str_replace('\\', '.', $trace[0]['function']) : '(main)',
-                $file,
-                $line === null ? '' : ':',
-                $line === null ? '' : $line);
-            if (is_array($seen)) {
-                $seen[] = "$file:$line";
-            }
-            if (!count($trace)) {
-                break;
-            }
-            $file = array_key_exists('file', $trace[0]) ? $trace[0]['file'] : 'Unknown Source';
-            $line = array_key_exists('file', $trace[0]) && array_key_exists('line', $trace[0]) && $trace[0]['line'] ? $trace[0]['line'] : null;
-            array_shift($trace);
-        }
-
-        $result = join("\n", $result);
-        if ($prev) {
-            $result .= "\n" . self::error2trace($prev, $seen);
-        }
-        return $result;
-    }
 }
