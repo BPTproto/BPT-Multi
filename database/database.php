b5c504566464d583d148dcaffa07a26212449d2c
Alireza <74488703+Alireza-ME@users.noreply.github.com>
2022-06-26 11:13:07 +0430

    Create database.php

---
+++ database/database.php
@@ -0,0 +1,75 @@
+<?php
+  
+ //define namespace
+ namespace BPT\Database;
+ 
+ //use classes
+ use Medoo\Medoo;
+ use BPT\Database\JsonDb;
+ 
+ 
+  /**
+   * @class Database
+   */
+  class Database
+  {
+        
+   /**
+   * @const types database
+   */
+   const TYPES = array('Mysqli','Medoo','Json');
+ 
+   /**
+    * @const Medoo database types
+    */
+   const Medoo_Types = array('mysql','mariadb','pgsql','sybase','oracle','mssql', 'sqlite');
+
+    /**
+     * @var type database
+     */
+    protected $type;
+      
+    /**
+     * @var host for database
+     * Default value localhost
+     */
+    protected $host;
+       
+    /**
+      * @var username database username 
+      */
+    protected $username;
+      
+    /**
+      * @var dbname database name
+      */
+    protected $dbname;
+       
+    /**
+      * @var charset database
+      */
+    protected $charset;
+        
+    /**
+      * @var password database password
+      */
+    protected $password;
+       
+    /**
+      * @var connection database
+      */
+    protected $connect;
+       
+      /**
+       * @method Json_init
+       * Create a Json database
+       */
+      protected function Json_init()
+      {
+        $JsonDb = new JsonDb();
+        $JsonDb->init($this->dbname);
+      }
+      
+      
+      
+  }
