6f73ba40fd3e42f690491859df79dd2d3c9f714d
Alireza <74488703+Alireza-ME@users.noreply.github.com>
2022-06-27 12:54:21 +0430

    update database.php

--- database/database.php
+++ database/database.php
@@ -1,72 +1,23 @@
 <?php
   
- //define namespace
  namespace BPT\database;
  
- //use classes
  use Medoo\Medoo;
- use BPT\database\jsondb;
- 
+ use BPT\database\{handller,jsondb};
  
   /**
    * @class Database
    */
   class database
   {
-        
-   /**
-   * @const types database
-   */
-   const TYPES = array('Mysqli','Medoo','Json');
- 
-   /**
-    * @const Medoo database types
-    */
-   const Medoo_Types = array('mysql','mariadb','pgsql','sybase','oracle','mssql', 'sqlite');
-
-    /**
-     * @var type database
-     */
-    protected $type;
-      
-    /**
-     * @var host for database
-     * Default value localhost
-     */
-    protected $host;
-       
-    /**
-      * @var username database username 
-      */
-    protected $username;
-      
-    /**
-      * @var dbname database name
-      */
-    protected $dbname;
-       
-    /**
-      * @var charset database
-      */
-    protected $charset;
-        
-    /**
-      * @var password database password
-      */
-    protected $password;
-       
-    /**
-      * @var connection database
-      */
-    protected $connect;
        
       /**
        * @method Json_init
-       * Create a Json database
+
        */
-      protected function Json_init()
+      public function json_init()
       {
-        $JsonDb = new JsonDb();
-        $JsonDb->init($this->dbname);
+       (new jsondb())->init(handller::$dbname);
       }
+      
    }
