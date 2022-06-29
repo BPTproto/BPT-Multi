7044f107a7c43c2b7cc67ba6a7754d7a1b07e691
Miaad Sadeghloo <miaadps@gmail.com>
2022-06-29 23:53:03 +0430

    Adding extra methods which remain fron BPTPROTO/BPT lib

---
+++ constants/cryptoAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BPT\constants;
+
+/**
+ * Different type of crypto action
+ */
+class cryptoAction {
+    public const ENCRYPT = 'encrypt';
+    public const DECRYPT = 'decrypt';
+}
