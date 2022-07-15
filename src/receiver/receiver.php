0a68c1701f317469ab17857dc714e37c388c785d
Miaad Sadeghloo <miaadps@gmail.com>
2022-07-15 22:57:08 +0430

    fix a simple bug

--- src/receiver/receiver.php
+++ src/receiver/receiver.php
@@ -35,7 +35,8 @@ class receiver {
                 BPT::exit();
             }
         }
-        elseif (!is_a($update,'update')) {
+
+        if (!is_a($update,'update')) {
             $update = new update($update);
         }
 
