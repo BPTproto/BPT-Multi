6fd55a763e8c006e49bb6a46e06418f17410b0d0
Miaad Sadeghloo <miaadps@gmail.com>
2023-05-02 13:23:00 +0330

    Add strReplace function

--- src/functions.php
+++ src/functions.php
@@ -18,3 +18,37 @@ namespace BPT;
 function object(... $argument): object {
     return (object) $argument;
 }
+
+/**
+ * Same as str_replace but with a small different
+ *
+ * str_replace usage :
+ * ```
+ * str_replace(
+ * [
+ *     'test1',
+ *     'test2'
+ * ],
+ * [
+ *     'test3',
+ *     'test4'
+ * ], $test_text);
+ * ```
+ *
+ * strReplace usage :
+ * ```
+ * strReplace(
+ * [
+ *     'test1' => 'test3',
+ *     'test2' => 'test4'
+ * ], $test_text);
+ * ```
+ *
+ * @param array        $replacements Contain key => value array for needle => replacement
+ * @param string|array $subject The string or array being searched and replaced on, otherwise known as the haystack.
+ *
+ * @return array|string
+ */
+function strReplace(array $replacements, string|array $subject): array|string {
+    return str_replace(array_keys($replacements), array_values($replacements), $subject);
+}
