386fb68aaf03c376279d86c1776ea5cfc202b781
Miaad Sadeghloo <miaadps@gmail.com>
2023-07-23 21:07:03 +0330

    A little optimization

--- src/logger.php
+++ src/logger.php
@@ -20,20 +20,11 @@ class logger {
     public static function init (int $log_size = 10): void {
         self::$log_size = $log_size;
         $log_file = realpath(settings::$name.'BPT.log');
-        if (file_exists($log_file) && !(filesize($log_file) > self::$log_size * 1024 * 1024)) {
-            $mode = 'a';
-            $write = false;
-        }
-        else {
-            $mode = 'w';
-            $write = true;
-        }
+        $mode = file_exists($log_file) && !(filesize($log_file) > self::$log_size * 1024 * 1024) ? 'a' : 'w';
         self::$handler = fopen(settings::$name.'BPT.log', $mode);
-
-        if ($write) {
+        if ($mode === 'w') {
             fwrite(self::$handler,"♥♥♥♥♥♥♥♥♥♥♥♥♥♥ BPT Library  ♥♥♥♥♥♥♥♥♥♥♥♥♥♥\nTnx for using our library\nSome information about us :\nAuthor : @Im_Miaad\nHelper : @A_LiReza_ME\nChannel : @BPT_CH\nOur Website : https://bptlib.ir\n\nIf you have any problem with our library\nContact to our supports\n♥♥♥♥♥♥♥♥♥♥♥♥♥♥ BPT Library  ♥♥♥♥♥♥♥♥♥♥♥♥♥♥\nINFO : BPT Library LOG STARTED ...\nwarning : this file automatically deleted when its size reached log_size setting, do not delete it manually\n\n");
         }
-
         if (self::$waited_logs != []) {
             foreach (self::$waited_logs as $log) {
                 fwrite(self::$handler, $log);
@@ -52,7 +43,7 @@ class logger {
      * @return void
      */
     public static function write(string $data, string $type = loggerTypes::NONE): void {
-        $text = date('Y/m/d H:i:s') . ( $type === loggerTypes::NONE ? " : $data\n\n" : " : ⤵\n$type : $data\n\n" );
+        $text = date('Y/m/d H:i:s') . ($type !== loggerTypes::NONE ? " : ⤵\n$type" : '') . " : $data\n\n";
         if (!is_null(self::$handler)) {
             fwrite(self::$handler, $text);
         }
